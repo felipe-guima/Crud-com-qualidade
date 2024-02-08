90079534683f28f4ef1eb85de61628df2b4234cb
Felipe Guimaraes <feelipe.guima@gmail.com>
2024-02-08 19:55:09 -0300

    melhoria das funções create() e read() com criação de interface para o objeto todo e FastFail Falidations para caso o arquivo de leitura 'banco de dados' esteja vazio não apresentando algo para parsear. Criação da clearDB

--- core/crud.ts
+++ core/crud.ts
@@ -1,27 +1,59 @@
 import fs from 'fs';
 const DB_FILE_PATH = "./core/db";
 
-// No entanto tudo isso é criado apenas em memoria sem percistencia
-
 console.log("----------------[CRUD]-------------------")
 
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
 function create(content: string) {
+  const todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  }
 
+  // diamond operator, modo de tipar a constante Array <Todo>
+  const todos: Todo[] = [
+    ...read(),
+    todo,
+  ]
+  
   // passar o caminho e o conteudo na funçao WriteFileSync
-  fs.writeFileSync(DB_FILE_PATH, content)
-
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos,
+  }, null, 2))
   return content;
 }
 
-function read() {
-  const db = fs.readFileSync(DB_FILE_PATH, "utf-8")
+// Fail Fast validations
+function read():Array<Todo> {
+  // garantir que caaso não venha um objeto o codigo não quebre
+  // talvez tenha string ou não
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}")
+  
+  if (!db.todos) {
+    return []
+  }
 
-  return db;
+  return db.todos;
 }
 
-//create("Primeira TODO do projeto !")
+function clearDB() {
+  fs.writeFileSync(DB_FILE_PATH, '')
+}
+
+//Chamadas de Funçes
+
+clearDB();
+
+create("-----PRIMEIRO TODO !!!!!------");
+create("Segunda TODO *****");
+
+console.log(read());
 
 
-create(" Nova TODO para ler o arquivo !")
-
-console.log(read())
