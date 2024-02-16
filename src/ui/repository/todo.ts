8893f5043eaa512e0370e3a27b1604031be08b30
Felipe Guimaraes <feelipe.guima@gmail.com>
2024-02-16 04:09:38 -0300

    desemvolvendo o repository da aplicação, garantindo o dado que sera passado com mais rigor.

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -16,9 +16,9 @@ function get({
     async (respostaDoServidor) => {
       const todosString = await respostaDoServidor.text();
       // Cast não recomendado no Back ex. as Todo[]
-      const todosFromServer = JSON.parse(todosString).todos;
-      console.log(page);
-      console.log(limite);
+      const todosFromServer = parseTodosFromServer(
+        JSON.parse(todosString)
+      ).todos;
 
       const ALL_TODOS = todosFromServer;
       const startIndex = (page - 1) * limite;
@@ -26,11 +26,11 @@ function get({
       const paginetedTodos = ALL_TODOS.slice(startIndex, endIndex);
       const totalPages = Math.ceil(ALL_TODOS.length / limite);
 
-      // como garantir que um dado não é any ser fazer o cast 
+      // como garantir que um dado não é any ser fazer o cast
       return {
-        todos: ALL_TODOS,
+        todos: paginetedTodos,
         total: ALL_TODOS.length,
-        pages: 1,
+        pages: totalPages,
       };
     }
   );
@@ -46,4 +46,39 @@ interface Todo {
   content: string;
   date: Date;
   done: boolean;
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+  if (
+    responseBody !== null &&
+    typeof responseBody === "object" &&
+    "todos" in responseBody &&
+    Array.isArray(responseBody.todos)
+  ) {
+    return {
+      todos: responseBody.todos.map((todo: unknown) => {
+        if (todo == null && typeof todo !== "object") {
+          throw new Error("invalid todo from server");
+        }
+
+        const { id, content, date, done } = todo as {
+          id: string;
+          content: string;
+          date: string;
+          done: string;
+        };
+
+        return {
+          id,
+          content,
+          done: String(done).toLowerCase() === "true",
+          date: new Date(date),
+        };
+      }),
+    };
+  }
+
+  return {
+    todos: [],
+  };
 }
